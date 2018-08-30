fb3905e4f2313809e6f5181d637a52c59a8db746
qumy <qumy@yandex.ru>
2018-08-30 14:40:08 +0500

    add project endpoint

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,12 +1,20 @@
 'use strict'
+const Project = use('App/Models/Project')
 
 class ProjectController {
-  index({ auth }) {
-    const user = await auth.getUser();
-    console.log(user)
-    return {
-      message: 'hello world'
-    }
+  async index({ auth }) {
+    const user = await auth.getUser()
+    return await user.projects().fetch()
+  }
+  async create({ auth, request }) {
+    const user = await auth.getUser()
+    const { title } = request.all()
+    const project = new Project()
+    project.fill({
+      title
+    })
+    await user.projects().save(project)
+    return project
   }
 }
 
--- server/start/routes.js
+++ server/start/routes.js
@@ -5,5 +5,6 @@ const Route = use('Route')
 Route.group(() => {
   Route.post('auth/login', 'UserController.login')
   Route.post('auth/register', 'UserController.register')
-  Route.get('projects', 'ProjectController.index')
+  Route.get('projects', 'ProjectController.index').middleware('auth')
+  Route.post('projects', 'ProjectController.create').middleware('auth')
 }).prefix('api')
