7e1c0dddf80d0e660a4ca3cb0140455ebf23e04a
qumy <qumy@yandex.ru>
2018-09-19 09:19:06 +0500

    set up a editable record template

---
+++ client/src/components/EditableRecord.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs9 class="text-xs-left">
+      <slot></slot>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
