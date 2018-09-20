0e8b534c84a01fe13661827f5cbd329b38ca49f1
qumy <qumy@yandex.ru>
2018-09-21 00:03:40 +0500

    added create record

--- client/src/components/EditableRecord.vue
+++ client/src/components/EditableRecord.vue
@@ -2,16 +2,30 @@
   <v-layout row wrap>
     <v-flex xs9 class="text-xs-left">
       <slot></slot>
+      <span
+        @click="$emit('onClick')"
+        v-if="!isEditMode">
+          {{title}}
+      </span>
+      <v-text-field
+        v-if="isEditMode
+        :value="title"
+        @keyup.enter="$emit('onSave')"
+        @input="$emit('onInput', $event)"
+        ></v-text-field>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
-
-}
+  props: [
+    'isEditMode',
+    'title',
+    'record',
+  ],
+};
 </script>
 
 <style>
-
 </style>
